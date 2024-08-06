4d50e1a615d3192fa67f5df63ea60437f9bc395a
Mabc365 <mustafaasif365@hotmail.com>
2024-08-06 17:05:14 -0400

    nvim setup

---
+++ setup-scripts/neovim-setup.ps1
@@ -0,0 +1,10 @@
+#Installing Neovim
+winget install Neovim.Neovim
+scoop install gcc
+
+#Installing Neovim Base Packages
+git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
+Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
+nvim
+
+#Installing Neovim theming
